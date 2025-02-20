fa99738bac3dbcb0951f9992c1f0edffd259cba8
Suneetha Rani <suneetha04011622@gmail.com>
2025-02-20 21:26:21 +0530

    terraform

---
+++ variables/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "this" {
+  ami                    = "var.ami_id"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  instance_type          = "var.instance_type"
+  tags = var.ec2_tags
+
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port   = var.from_port
+    to_port     = var.to_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # -1 means allow all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags =var.sg_tags
+}
+
+
---
+++ variables/variables.tf
@@ -0,0 +1,43 @@
+variable  "ami_id" {
+  type = string
+  default ="ami-09c813fb71547fc4f"
+  description ="This is RHEL9 AMI ID"
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+}
+
+variable "ec2_tags" {
+  type = map
+  default = {
+    project = "Expense"
+    component = "backend"
+    Environment = "dev"
+    Name  = "expense-backend-dev"
+  }
+}
+
+variable "from_port" {
+    type = number
+    default = 22
+
+}
+
+variable "to_port" {
+    type = number
+    default = 22
+}
+
+variable "cidr_blocks" {
+    type = list
+    default = ["0.0.0.0/0"] 
+}
+
+variable "sg_tags" {
+    type = map 
+    default = {
+        Name = "expense-backend-dev"
+    } 
+    
+}
